77b931a297164ba5c0f3571314bd02a3964110a1
atomarxculo <atomarxculo@users.noreply.github.com>
2025-12-16 20:26:16 +0000

    deploy: 8a81175412e8fa0bbba863a72c01fa7a50cb4974

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765916712',resources: [
+    cacheName: 'chirpy-1765916775',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
